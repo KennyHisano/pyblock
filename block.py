fc2faa371d93c6a7e2e9a65abfd7fb44c23e1ff5
NandN <NandN@inandn.local>
2017-09-05 16:38:07 +0900

    block and its head

--- block.py
+++ block.py
@@ -7,7 +7,7 @@ import hashlib
 class blockHead():
 	
 
-    def __init__(self,ver,prev_hash,merkRoot,timestamp,diffic,nonce):
+    def __init__(self,ver,prev_hash,merkRoot,timestamp,diffic=0,nonce=0):
         self.ver = ver
         self.prev_hash = prev_hash
         self.merkRoot = merkRoot
@@ -15,4 +15,18 @@ class blockHead():
         self.diffic = diffic
         self.nonce = nonce 
 
+
+    def __len__(self):
+    	return 'Nonce {}'.format(self.nonce)
 #    def hash(self):
+
+
+
+
+class block(blockHead):
+
+
+	def __init__(self,size,txcount,tx):
+		self.size = size
+		self.txcount = txcount
+		self.tx = tx
